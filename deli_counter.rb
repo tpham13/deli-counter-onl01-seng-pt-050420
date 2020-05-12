2ad9fa55b1783af0dee42b7a9c8a019dbf958ab8
Thanh Pham <tpham0123@gmail.com>
2020-05-12 22:55:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,3 @@
-def line (katz_deli)
-  puts "The line is currently empty."
-  
-end 
-
-
-def take_a_number (katz_deli,name)
-   if katz_deli.length > 0 
-    puts "The line is currently: #{katz_deli.length}."
-end   
+def line(deli)
+  if deli.length == 0 
+    puts "The line is currently empty."
