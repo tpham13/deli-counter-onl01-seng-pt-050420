f766a9fa8b72b373bc2776a30e2c51bc246d1a27
Thanh Pham <tpham0123@gmail.com>
2020-05-12 22:59:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,14 @@
 def line(deli)
   if deli.length == 0 
     puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each_with_index do |customer, index|
+      current_line << "#{index + 1}, #{customer}""
+    
+    end
+    puts current_line 
   end
 end
+
+
