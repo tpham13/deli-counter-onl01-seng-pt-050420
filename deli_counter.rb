bc9a5d5d033132921f282594ef7631b4c8ec6191
Thanh Pham <tpham0123@gmail.com>
2020-05-12 23:01:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ def line(deli)
   else
     current_line = "The line is currently:"
     deli.each_with_index do |customer, index|
-      current_line << "#{index + 1}, #{customer}""
-    
+      current_line << " #{index + 1}. #{customer}"
     end
     puts current_line 
   end
